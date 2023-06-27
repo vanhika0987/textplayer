b5c83770472d8c72c375f17f323e38deb0a0f760
Vanshika <vanshika.kumari2080@gmail.com>
2023-06-28 03:18:59 +0530

    navbar color change components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 
 
 function App() {
   return (
 
     <>
+    <Navbar   title = "textutils"  mainpage= "HOME" aboutapp ="ABOUT US" >   </Navbar>
 
     
     
