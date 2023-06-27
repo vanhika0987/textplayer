7fd13815a01829e670c825f5db17cdeb901b5e20
Vanshika <vanshika.kumari2080@gmail.com>
2023-06-28 02:23:17 +0530

    navbar component chang

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
               <li><a className="dropdown-item" href='/'>Action</a></li>
               <li><a className="dropdown-item" href='/'>Another action</a></li>
               <li><hr className="dropdown-divider"/></li>
-              <li><a className="dropdown-item" href='/'>Something else here</a></li>
+              <li><a className="dropdown-item" href='/'>contact us</a></li>
             </ul>
         </li>
           <li className="nav-item">
